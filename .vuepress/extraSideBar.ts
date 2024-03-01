02746ecd6084c33616504fd9f95c74b23e93a0b6
wyxh2004 <103009253+WeiYuXingHan@users.noreply.github.com>
2024-03-01 21:02:05 +0800

    just a simplification

--- .vuepress/extraSideBar.ts
+++ .vuepress/extraSideBar.ts
@@ -11,32 +11,6 @@ export default [
     popoverDesc: "可以手机看或分享至朋友圈",
   },
   {
-    title: "星球",
-    icon: "/icon/xingqiu.png",
-    popoverTitle:
-      '<span style="font-size:0.8rem;font-weight:bold;"><span style="color:red;">保姆级实战项目教程</span>、编程学习指南、学习资源、求职指南、技术分享、编程交流</span>',
-    popoverUrl:
-      "/qrcode-codenav.png",
-    popoverDesc: "知识星球：编程导航",
-  },
-  {
-    title: "交流群",
-    icon: "/icon/weixin.png",
-    popoverTitle:
-      '<span style="font-size:0.8rem;font-weight:bold;">扫码添加 <span style="color:red;">编程导航小助手微信</span>，拉你进专属编程学习交流群</span>',
-    popoverUrl:
-      "/qrcode-codenavhelper.png",
-  },
-  {
-    title: "下资料",
-    icon: "/icon/xiazai.png",
-    popoverTitle:
-      '<span style="font-size:0.8rem;font-weight:bold;">扫码关注站长公众号，回复 <span style="color:red;">学习</span> 获取海量编程学习资源「无任何套路」</span>',
-    popoverUrl:
-      "/qrcode-mpcoder_yupi.jpg",
-    popoverDesc: "公众号: 微雨星晗",
-  },
-  {
     title: "支持我",
     icon: "/icon/dianzan.png",
     popoverTitle:
@@ -44,6 +18,6 @@ export default [
     popoverUrl:
       "/qrcode-thumb.jpg",
     popoverDesc:
-      "感谢您的支持，作者头发++",
+      "感谢您的支持",
   },
 ];
